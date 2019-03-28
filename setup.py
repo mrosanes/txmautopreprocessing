4f04c3128948b6d0c22b96a272e971deb75db21b
mrosanes <mrosanes@cells.es>
2019-03-28 09:55:49 +0100

    Bump version 1.0.0-alpha to 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '1.0.0-alpha'
+__version = '1.0.0'
 
 long_description = """This is a DS to execute MISTRAL preprocessing Workflows
 """
