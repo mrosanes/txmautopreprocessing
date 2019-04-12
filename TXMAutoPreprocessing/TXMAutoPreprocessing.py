b652869efc523307c7eafe70027caf6f5d38986e
mrosanes <mrosanes@cells.es>
2019-04-12 16:30:40 +0200

    Set symlink to parent folder in set_TXM_file
    Set symlink to parent folder in set_TXM_file method,
    in order to improve UX. Allowing BL09 scientists
    to set (in the first line edit from XMController),
    the file /.../BL09_RAWDATA/file.txt and from it, the second
    line edit will be directly filled with /.../BL09_RAWDATA.

--- TXMAutoPreprocessing/TXMAutoPreprocessing.py
+++ TXMAutoPreprocessing/TXMAutoPreprocessing.py
@@ -98,12 +98,19 @@ class TXMAutoPreprocessing(Device):
             self.user_folder_relative_path = self._root_folder
 
     @DebugIt(show_args=True)
-    def set_TXM_file(self, txmfile):
+    def set_TXM_file(self, txmfile, set_link=True):
         if "/storagebls" in txmfile:
             txmfile = txmfile.replace("/storagebls", "")
         self.debug_stream("Set TXM_file: %s" % txmfile)
         self._root_folder = os.path.dirname(txmfile)
         self._txm_file = txmfile
+
+        if set_link:
+            self.root_folder_relative_path = self._root_folder.replace(
+                "/beamlines/bl09", "..")
+            os.system("rm %s" % self._all_files_link)
+            os.system("ln -s %s %s" % (self.root_folder_relative_path,
+                                       self._all_files_link))
 
     def get_TXM_file(self):
         return self._txm_file
@@ -241,6 +248,9 @@ class TXMAutoPreprocessing(Device):
             # For Folder select and others
             # Setting default link, otherwise from Windows, if link is
             # broken, the link cannot be seen.
+            self._root_folder = "/beamlines/bl09/controls/DEFAULT_USER_FOLDER"
+            self.root_folder_relative_path = self._root_folder.replace(
+                "/beamlines/bl09", "..")
             os.system("rm %s" % self._all_files_link)
             os.system("ln -s %s %s" % (self.root_folder_relative_path,
                                        self._all_files_link))
@@ -251,6 +261,9 @@ class TXMAutoPreprocessing(Device):
             # For Folder select and others
             # Setting default link, otherwise from Windows, if link is
             # broken, the link cannot be seen.
+            self._root_folder = "../controls/DEFAULT_USER_FOLDER"
+            self.root_folder_relative_path = self._root_folder.replace(
+                "/beamlines/bl09", "..")
             os.system("rm %s" % self._all_files_link)
             os.system("ln -s %s %s" % (self.root_folder_relative_path,
                                        self._all_files_link))
